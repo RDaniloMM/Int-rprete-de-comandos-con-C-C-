edc2c0591215d4d2316b8eb2a32f2e201e332f23
CarlosPercca <carlos.percca.13@gmail.com>
2023-12-15 09:06:26 -0500

    comentarios 30%

--- prueba2.cpp
+++ prueba2.cpp
@@ -20,34 +20,16 @@
 #define MAX_ARGUMENTS 32
 using namespace std;
 
-//(com (-opcs)* (args)* (opr in/outfile?)*)*
-//Ejemplo:
-//mkdir Prueba && cd Prueba && touch proy.txt in.txt && ls -al > proy.txt && ./prueba <in.txt >out.txt
-/*
-Lista de operadores de control:
-    Operadores de redireccion:
-        Solo se acepta uno de ellos a la vez, se evalÃºa el caso.
-        >
-        <
-        |
-    Operadores de encadenamiento:
-        ;
-        &&
-        ||
-*/
 
 class Comando{
 private:
-    //char *outfile;
-    //char *inputfile;
     char dir[PATH_MAX];
     char hostName[HOST_NAME_MAX];
     passwd *user;
     queue<pair<char **, int>> coms; //cola de comandos a ejecutarse
     queue<char *> direccion; //cola de archivos o argumentos
     char str[MAX_COMMAND_LENGTH]; //comando de entrada
-    vector<int> mod;
-    //int background;
+    vector<int> mod; // indicador de si el comando tiene redireccion para eliminar direcciones residuales
 
 public:
     Comando();
@@ -84,52 +66,32 @@ void Comando::prompt(){
 //comando 
 void Comando::divide(){
     // se divide la linea en argumentos, args[0] es el comando simple
-    {
-    //cuando no hay encadenamiento:
-    //1. ls
-    //2. ls -al
-    //Se acabo, ya que no hay encadenamiento:
-    //3. ls -al NULL
-    //4. coms.push(comsimple)
-
-    //cuando si hay encad:
-    //1. ls
-    //2. ls -al
-    //Se detecta "&&" por lo que:
-    //3. ls -al NULL
-    //4. coms.push(comsimple)
-    //se reinicia comsimple
-    //5. pwd
-    //
-    }
     int n = 0, casoencad, casoredir = 0;
     bool hayencad = 0, haydirec = 0;
 
-    char **comsimple = new char *[MAX_ARGUMENTS];
-    char *argredireccion;
-    char *token = strtok(str, " ");
+    char **comsimple = new char *[MAX_ARGUMENTS]; //Se encarga de reunir los comandos token por token
+    char *token = strtok(str, " "); //Asigna a cada token los valores de ingreso del str separados por " " espacio
     while(token != NULL && n < MAX_COMMAND_LENGTH){
-        casoredir = 0;
+        casoredir = 0; // Regresa a 0 para volver a empezar denuevo a analizar el redireccionamiento
         if(!(casoencad = casoencadenamiento(token))){ //si no es un opr de encad, se sigue guardando como comando simple
-            casoredir = casoredireccion(token);
-
-            if(casoredir){
-                haydirec = 1;
-                token = strtok(NULL, " ");
-                comsimple[n] = NULL;
-                coms.push({comsimple,casoredir});
-                direccion.push(token);
-                mod.push_back(casoredir);
+            casoredir = casoredireccion(token); //extrae el valor 0 si no hay redireccion , 1 si encuentra > , 2 si encuntra <
+            if(casoredir){ // caso encuentre redireccion
+                haydirec = 1; //cambia la variable a que si encontro redireccion
+                token = strtok(NULL, " "); //pasa al siguiente token obviando ">,<" para que token almacene el nombre del archivo
+                comsimple[n] = NULL; //pone limite al conjunto de tokens antes de ">,<" almacenados en comsimple
+                coms.push({comsimple,casoredir}); // se inserta todo el comando antes de ">,<" y que tipo de redireccion es (casoredir);
+                direccion.push(token);// almacena el nombre del archivo que va despues de ">,<" gracias a que se uso la linea 80
+                mod.push_back(casoredir);// De acuerdo a la posicion del comando esto almacenara si es una redireccion o un comando simple
             }
             else{
-                comsimple[n++] = token;
+                comsimple[n++] = token; // guarda cada token dentro de comsimple
             }
         }
         else{//caso contrario, se acaba el comando simple y se guarda en la cola de comandos
-            hayencad = 1;
-            comsimple[n] = NULL;
-            if(!haydirec){
-                coms.push({comsimple,casoredir});
+            hayencad = 1; // Creamos este auxiliar para saber si la entrada tiene encadenamiento
+            comsimple[n] = NULL; // pone limite al conjunto de tokens almacenados
+            if(!haydirec){ // consulta si anterior a ello se encontro un direccionamiento porque si encontro se volveria a repetir la linea 94
+                coms.push({comsimple,casoredir});// caso que no encontro redireccionamiento significa que la linea 82 no fue ejecutada, por ende necesita ejecutarse para guarda el comando simple
                 mod.push_back(0);
             }
             haydirec = 0;
@@ -143,6 +105,9 @@ void Comando::divide(){
     if(!haydirec){
         coms.push({comsimple,casoredir});
         mod.push_back(0);
+    }
+    for (int i = 0 ; mod.size() ; i++){
+        cout<<mod[i]<<" ";
     }
 }
 
@@ -170,9 +135,9 @@ void Comando::execute(){
             }
             if(!direccion.empty() && (mod[0] > 0)) {
                 direccion.pop();
-            }
-            mod.erase(mod.begin());
+            }  
         }
+        mod.erase(mod.begin());
         coms.pop();
     }
 }
@@ -195,7 +160,7 @@ void Comando::processRed(){ // Analiza si se ejecuto alguna redireccion para el 
             dup2(fd, STDIN_FILENO); // lee el archivo en lugar del teclado y lo reserva
             close(fd); // cierra el archivo
 
-        }else if(coms.front().second == 1){
+    }else if(coms.front().second == 1){
             int fd = open(direccion.front(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);  	 
             if (fd < 0) {
                 cerr << "Error al abrir el archivo de salida" << std::endl;
