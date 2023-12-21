39370f7670f0fd332d083139a27af226faca0d9f
CarlosPercca <carlos.percca.13@gmail.com>
2023-12-20 20:49:20 -0500

    Eliminaciomn de vector mod

--- prueba2.cpp
+++ prueba2.cpp
@@ -30,7 +30,6 @@ private:
     queue<pair<char **, int>> coms; //cola de comandos a ejecutarse
     queue<char *> direccion; //cola de archivos o argumentos
     char str[MAX_COMMAND_LENGTH]; //comando de entrada
-    vector<int> mod; //vector de redirecciones
 public:
     Comando();
     void prompt();
@@ -69,42 +68,41 @@ void Comando::divide(){
     int n = 0, casoencad, casoredir = 0;
     bool haydirec = 0;
 
-    char **comsimple = new char *[MAX_ARGUMENTS]; //Se encarga de reunir los comandos token por token
-    char *token = strtok(str, " "); //Asigna a cada token los valores de ingreso del str separados por " " espacio
+    char **comsimple = new char *[MAX_ARGUMENTS]; 
+    char *token = strtok(str, " ");
     while(token != NULL && n < MAX_COMMAND_LENGTH){
         casoredir = 0; // Regresa a 0 para volver a empezar denuevo a analizar el redireccionamiento
 
-        if(!(casoencad = casoencadenamiento(token))){ //si no es un opr de encad, se sigue guardando como comando simple
-            casoredir = casoredireccion(token); //extrae el valor 0 si no hay redireccion , 1 si encuentra > , 2 si encuntra <
-            if((casoredir == 1) || (casoredir == 2) || (casoredir == 3)){ // caso encuentre redireccion
-                haydirec = 1; //cambia la variable a que si encontro redireccion
-                token = strtok(NULL, " "); //pasa al siguiente token obviando ">,<" para que token almacene el nombre del archivo
-                comsimple[n] = NULL; //pone limite al conjunto de tokens antes de ">,<" almacenados en comsimple
-                coms.push({comsimple,casoredir}); // se inserta todo el comando antes de ">,<" y que tipo de redireccion es (casoredir);
-                direccion.push(token);// almacena el nombre del archivo que va despues de ">,<" gracias a que se uso la linea 80
-                mod.push_back(casoredir);// De acuerdo a la posicion del comando esto almacenara si es una redireccion o un comando simple
+        if(!(casoencad = casoencadenamiento(token))){ 
+            casoredir = casoredireccion(token); 
+            if((casoredir == 1) || (casoredir == 2) || (casoredir == 3)){
+                haydirec = 1; 
+                token = strtok(NULL, " "); 
+                comsimple[n] = NULL; 
+                coms.push({comsimple,casoredir}); 
+                direccion.push(token);
             }else if(casoredir == 4){
                 comsimple[n] = NULL;
                 coms.push({comsimple,casoredir});
-                mod.push_back(casoredir);
                 haydirec = 0;
                 n = 0;
                 comsimple = new char *[MAX_ARGUMENTS];
+                direccion.push(NULL);
             }
             else{
                 comsimple[n++] = token; // guarda cada token dentro de comsimple
             }
         }
 
-        else{//caso contrario, se acaba el comando simple y se guarda en la cola de comandos
-            comsimple[n] = NULL; // pone limite al conjunto de tokens almacenados
-            if(!haydirec){ // consulta si anterior a ello se encontro un direccionamiento porque si encontro se volveria a repetir la linea 94
-                coms.push({comsimple,casoredir});// caso que no encontro redireccionamiento significa que la linea 82 no fue ejecutada, por ende necesita ejecutarse para guarda el comando simple
-                mod.push_back(0);
+        else{
+            comsimple[n] = NULL; 
+            if(!haydirec){ 
+                coms.push({comsimple,casoredir});
             }
             haydirec = 0;
             n = 0;
             comsimple = new char *[MAX_ARGUMENTS];
+            direccion.push(NULL);
         }
 
         token = strtok(NULL, " ");
@@ -113,7 +111,7 @@ void Comando::divide(){
     comsimple[n] = NULL;
     if(!haydirec){
         coms.push({comsimple,casoredir});
-        mod.push_back(0);
+        direccion.push(NULL);
     }
 }
 
@@ -141,11 +139,8 @@ void Comando::execute(){
             else{// Proceso padre
                 wait(NULL); //Espera a que el proceso hijo termine
             }
-            if(!direccion.empty() && (mod[0] > 0 && mod[0] != 4)){
-                direccion.pop();
-            }
         }
-        mod.erase(mod.begin());
+        direccion.pop();
         coms.pop(); 
     }
 }
@@ -215,6 +210,7 @@ void Comando::Pipe(){
             close(fd[WRITE_END]);
             dup2(fd[READ_END],STDIN_FILENO);
             close(fd[READ_END]);
+            direccion.pop();
             coms.pop();
             execvp(coms.front().first[0], coms.front().first); //Ejecuta el comando con sus argumentos
             cout << "Error al ejecutar el comando\n"; //cuando el comando se ejecuta correctamente no se muestra esta lÃ­nea
@@ -223,7 +219,7 @@ void Comando::Pipe(){
             close(fd[READ_END]);
             wait(NULL);
             wait(NULL);
-            mod.erase(mod.begin());
+            direccion.pop();
             coms.pop();
         }
     }
